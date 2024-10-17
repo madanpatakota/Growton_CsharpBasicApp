5a06ea7052fbc4496dc9a221c3a77b7676f55462
Madn <madan.patakota@gmail.com>
2024-10-17 20:16:00 +0530

    octbor-18

---
+++ CsharpBasicApp/3.Operators.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+class Operators
+{
+ 
+
+    // As the developer you must aware of the data.
+
+    //void methods
+    static void Main()
+    {
+
+        //Arthamatic operators + , 1 , * , / , %
+        int sum = 5 + 3;
+        //Console.WriteLine(sum); //8
+
+        int difference = 10 - 4; // difference is 6
+
+        int product = 3 * 4; // product is 12
+
+        decimal result = 3.17m * 2.17m;
+
+        int result1 = 10 / 2; //5
+
+        decimal remainder = 10 % 3; //1
+
+
+        //Comparsion operators  ,  == , != , > , < , >= , <=
+
+
+        //bool isEqual = 5 == 1; // false
+        //Console.WriteLine(isEqual);
+
+        // 5 is not equal to 1
+
+        //bool isEqual = 5 != 5;  // true
+        //Console.WriteLine(isEqual);
+
+        bool isGreater = (8 > 5); // true
+
+        bool isLess = (3 < 7);    // true
+
+        bool isGreaterOrEqual = (8 >= 8); //true
+
+        bool isLessOrEqual = (3 <= 7);  // true
+
+
+        //Logical operators  && , !! , !
+
+        /*
+         *    True && True -> T 
+         *    True && false -> F
+         *    false && true -> F
+         *    false && false -> f
+         *    
+         *    
+         *    True  || True -> T 
+         *    True  || false -> T
+         *    false || true  -> T
+         *    false || false -> f
+         *    
+         *    
+         *    !T  -> f
+         *    !f  -> T
+         *    
+         */
+
+        //bool isEqual1 = (5 > 100) && (3 == 3); //True && True->T
+        //Console.WriteLine($" isequal 1 : {isEqual1}"); //true
+
+
+        //bool isEqual2 = (5 > 100) || (3 == 3); //True && True->T
+        //Console.WriteLine($" isequal 2 : {isEqual2}"); //true
+
+        bool isEqual3 =  !(3 <= 3);
+        //Console.WriteLine($" isequal 3 : {isEqual3}");
+
+
+
+
+       //nullbale types
+
+      //  int age = 0;   0-9  0
+
+
+      // int? age = null;
+
+        int? age = null;
+        Console.WriteLine(age);
+
+        bool? nullableBool = null;
+
+        Console.ReadLine();
+
+    }
+}
+
---
+++ CsharpBasicApp/4.DatatypeConversation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class DatatypeConversation
+{
+    static void Main()
+    {
+
+        string age = "25";
+        //int cage = int.Parse(age);
+        int cage = Convert.ToInt32(age);
+        Console.WriteLine(cage);
+
+        double value   = 3.99;
+        int cvalue = (int)value;   //convert
+        Console.WriteLine(cvalue);
+
+
+        int salary = 20000;
+        string ssalary = salary.ToString();
+
+
+        Console.WriteLine(DateTime.Now);
+        string todayDate = DateTime.Now.ToString();
+
+
+        string sName = null;
+
+        //int cName1 = int.Parse(sName);
+        //Console.WriteLine(cName1);
+
+        int cName2 = Convert.ToInt32(sName);
+        Console.WriteLine(cName2);
+        //int cage = Convert.ToInt32(age);
+
+        Console.ReadLine();
+
+
+    }
+}
+
