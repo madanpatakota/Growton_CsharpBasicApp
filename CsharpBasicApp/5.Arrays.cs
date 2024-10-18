dc561a467657cc8c499489e960b26f81cdc862e5
Madn <madan.patakota@gmail.com>
2024-10-18 20:21:26 +0530

    code

---
+++ CsharpBasicApp/5.Arrays.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+//put debugger(break point) and use f10 -> step over
+
+class Arrays
+{
+
+    static void Main()
+    {
+        //int a = 1;
+
+        int[] numbers = new int[2];
+
+        numbers[0] = 10;
+        numbers[1] = 20;
+
+        //numbers[2] = 30;//error
+
+
+        int firstNumber = numbers[0];
+        Console.WriteLine(firstNumber);
+
+
+        //  Logic
+       
+        //                  0   1   2  3    4
+        int[] prices = { 10, 20, 30, 40, 50 };
+        int secondNumber = prices[1];
+        Console.WriteLine(secondNumber);
+
+        int thirdNumber = prices[2]; // 30
+        //200
+        Console.WriteLine($"Before modify{thirdNumber}");
+
+        prices[2] = 200;
+
+        foreach(int price in prices)
+        {
+            Console.WriteLine(price);   // 10 , 20 , 200 , 40 , 50;
+        }
+
+
+
+
+
+        // General Example
+
+        //Books  - English , Maths , sanskrt
+        ////foreach --> Every
+        foreach (int price in prices)
+        {
+            Console.WriteLine($"fruit price is - {price + 1000}");
+        }
+
+
+        string[] books = { "English", "Maths", "Sanskrit", "Hindi" };
+
+        int length = books.Length;
+        Console.WriteLine(length); // 4
+
+
+
+        //loop
+        //what is our task. print every book. then i need to pick everybook
+                               //target
+        foreach(string book in books)
+        {
+            Console.WriteLine(book + "has been completed." );
+        }
+
+
+
+
+
+
+        Console.ReadLine();
+
+    }
+
+}
+
---
+++ CsharpBasicApp/6.ConditionalStatements.cs
@@ -0,0 +1,82 @@
+﻿using System;
+class ConditionalStatements
+{
+
+    //Think as per client mindset....
+
+    //if elseif else switch
+
+    // yes -> then go head
+    // no  -> you are not allowed
+
+    static void Main()
+    {
+        //string studentName = "sbdjkvasdfas";
+
+        // //5 < 10   5
+
+        // if(studentName == "Siva")
+        // {
+        //     Console.WriteLine("Hey!!!! I am siva");
+        // }
+        // else if(studentName == "Karthik")
+        // {
+        //     Console.WriteLine("Hey!!!! I am Karthik");
+        // }
+        // else if(studentName == "Keerthi")
+        // {
+        //     Console.WriteLine("Hey!!!! I am Keerthi");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("Expected Student was not found!!!!!!");
+        // }
+
+        // Console.WriteLine("Completed Task!!!");
+
+
+
+
+        string studentName = "12345";
+
+        switch (studentName)
+        {
+            case "Keerthi":
+                {
+                    Console.WriteLine("Hey !! iam keerthi");
+                    break;
+                }
+            case "Karthik":
+                {
+                    Console.WriteLine("Hey !! iam Karthik");
+                    break;
+                }
+            case "Siva":
+                {
+                    Console.WriteLine("Hey !! iam Siva");
+                    break;
+                }
+            default:
+                {
+                    Console.WriteLine("no student found");
+                    break;
+                }
+
+
+        }
+
+
+        Console.WriteLine("Completed Task!!!");
+
+
+        Console.ReadLine();
+
+    }
+
+
+    // > < == != 
+
+
+
+}
+
